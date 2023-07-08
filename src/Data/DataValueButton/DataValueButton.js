239c6281d5d5d24da7d2fe739f286d7fc287993b
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-07-08 15:46:50 +0330

    create components DataValue button done

---
+++ src/Data/DataValueButton/DataValueButton.js
@@ -0,0 +1,22 @@
+const dataValueButtonNumber =[
+    {id:13,valueNumber:'Clear'},
+    {id:14,valueNumber:'C'},
+    {id:16,valueNumber:'÷'},
+    {id:9,valueNumber:'7'},
+    {id:10,valueNumber:'8'},
+    {id:11,valueNumber:'9'},
+    {id:15,valueNumber:'×'},
+    {id:5,valueNumber:'4'},
+    {id:6,valueNumber:'5'},
+    {id:7,valueNumber:'6'},
+    {id:17,valueNumber:'-'},
+    {id:2,valueNumber:'1'},
+    {id:3,valueNumber:'2'},
+    {id:4,valueNumber:'3'},
+    {id:18,valueNumber:'+'},
+    {id:1,valueNumber:'0'},
+    {id:12,valueNumber:'.'},
+    {id:19,valueNumber:'='},
+   
+]
+export default dataValueButtonNumber
